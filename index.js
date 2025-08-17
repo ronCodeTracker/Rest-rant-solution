3990defba5f3a36ffde79f651ef7ba7325cff142
Ronald K <2fixelectronics777@gmail.com>
2025-08-17 15:37:10 -0500

    Revert "netlify mod"
    This reverts commit 0fd153139ad4ca59d3f17ee7179b8dc5a2359490.

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 // Modules and Globals
 require('dotenv').config()
 const express = require('express')
-const serverless = require("serverless-http");
 const methodOverride = require('method-override')
 const app = express();
 
@@ -11,7 +10,6 @@ app.engine('jsx', require('express-react-views').createEngine())
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
-app.use("/.netlify/functions/app", app);
 
 // Controllers & Routes
 
